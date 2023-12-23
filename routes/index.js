6f38316399f8045c6cd0dcf6e158a1495aaddd8a
Tushar1603 <105315307+Tushar1603@users.noreply.github.com>
2023-12-23 08:49:34 -0500

    decorative changes
    1. added a .spacer class to index for adding spaces withing the section between different headings.
    2. changed the color of the highlighted text in the mark tag using .container p mark{} style text.
    3. added new text fields in the about section.
    
    in my header.ejs
    1. moved the heading to the bottom to fix the navbar and nav links on the top
    2. changed the toggle button icon from light bulb to a 🔆 emoji
    3. added a fixed-top in the navbar class to fix it to the top of the page.
    
    In routes/index.js
    i removed all the unused routes as i started using a scrollable page instead of a traditional design.
    
    1. future plan: planning to add more texts to the sections.
    2. planning to add some images and highlight some more words with different colors.
    3. planning to add some animations to make the page look even better.

--- routes/index.js
+++ routes/index.js
@@ -6,16 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/index', function(req, res, next) {
-  res.render('index', { title: 'Home' });
-});
-
-router.get('/portfolio', function(req, res, next) {
-  res.render('portfolio', { title: 'My portolio' });
-});
-
-router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'Contact Me' });
-});
-
 module.exports = router;
