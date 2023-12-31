f7215234757b80de06a5cf24ebce6fb7cac6e890
Tushar1603 <105315307+Tushar1603@users.noreply.github.com>
2023-12-31 11:43:02 -0500

    webkit scroller added.
    Added the webkit scroller to the original index.ejs. Got rid of all the xyz file and anything linked to it in other files to decrease complexity of the code.

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/xyz', function(req, res, next) {
-  res.render('xyz', { title: 'Express' });
-});
-
-
 module.exports = router;
