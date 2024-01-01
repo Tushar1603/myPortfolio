b63ba77bc61b1962eb7fcf4f65732f9af1f51a34
Tushar1603 <105315307+Tushar1603@users.noreply.github.com>
2023-12-31 20:50:44 -0500

    app.js update and index update
    updated port section to add multiple port options,
    updated meta keywords and downloadable pdf's

--- app.js
+++ app.js
@@ -39,8 +39,10 @@ app.use(function(err, req, res, next) {
 });
 
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 
